8a0e71e98e5f46342f82e2acb7b4104826a5cea5
RubenBoone <ruben.boone01@gmail.com>
2025-07-19 17:01:36 +0200

    basic product structure

--- app.py
+++ app.py
@@ -15,6 +15,43 @@ def get_db():
 @app.route("/test")
 def test_api():
     return jsonify({"message": "API is working!"})
+
+@app.route("/products", methods=["GET"])
+def get_products():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM products ORDER BY program ASC, name ASC")
+    products = cursor.fetchall()
+    conn.close()
+    return jsonify([dict(product) for product in products])
+
+def initialize_database():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS products (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        program INTEGER NOT NULL,
+                        name TEXT NOT NULL NULL,
+                        quantity INTEGER NOT NULL NULL DEFAULT 0,
+                        order_warning INTEGER NOT NULL DEFAULT 0,
+                        serial TEXT NOT NULL)''')
+    conn.commit()
+
+    # Check if table is empty before inserting
+    cursor.execute("SELECT COUNT(*) FROM products")
+    if cursor.fetchone()[0] == 0:
+        cursor.executemany('''INSERT INTO products (name, program, quantity, serial) VALUES (?, ?, ?, ?)''', [
+            ('Ice Cream', 10, 0, "00000"),
+            ('Frozen Vegetables', 20, 8, "00001"),
+            ('Frozen Pizza', 15, 19, "00002"),
+            ('Frozen Rucola', 15, 19, "00003")
+        ])
+        conn.commit()
+    
+    conn.close()
+
 if __name__ == '__main__':
+    print("Initializing database...")
+    initialize_database()
     print("Backend is up")
     app.run(host="0.0.0.0", port=7001)
